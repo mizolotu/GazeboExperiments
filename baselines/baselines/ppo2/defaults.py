eb6b4c69d84e931e46e4a27b39ed9d6cae285530
mizolotu <38492363+mizolotu@users.noreply.github.com>
2019-11-13 14:54:09 +0200

    13112019

--- baselines/baselines/ppo2/defaults.py
+++ baselines/baselines/ppo2/defaults.py
@@ -45,7 +45,7 @@ def mara_mlp():
         total_timesteps = 8640000 * 5,
         save_interval = 10,
         env_name = 'MARARandomTarget-v0',
-        transfer_path = None,
+        transfer_path = 'checkpoints/best',
     )
 
 def mara_lstm():
