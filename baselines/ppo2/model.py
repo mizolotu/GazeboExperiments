0bb4384023e2f174443fe3046577018ab880156e
mizolotu <38492363+mizolotu@users.noreply.github.com>
2020-01-29 11:33:13 +0200

    29.1.2020

--- baselines/ppo2/model.py
+++ baselines/ppo2/model.py
@@ -27,8 +27,6 @@ class Model(object):
     """
     def __init__(self, *, policy, ob_space, ac_space, nbatch_act, nbatch_train, nsteps, ent_coef, vf_coef, max_grad_norm, mpi_rank_weight=1, comm=None, microbatch_size=None):
 
-        tf.reset_default_graph()
-
         self.sess = sess = get_session()
 
         if MPI is not None and comm is None:
